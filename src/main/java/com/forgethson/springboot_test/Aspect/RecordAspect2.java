8bd418123f7ff0b1aa6fbf24ecb28283f8de78e1
Forgethson-RF Center <649343453@qq.com>
2024-09-01 23:21:22 +0800

    beautify code style

--- src/main/java/com/forgethson/springboot_test/Aspect/RecordAspect2.java
+++ src/main/java/com/forgethson/springboot_test/Aspect/RecordAspect2.java
@@ -1,18 +1,11 @@
 package com.forgethson.springboot_test.Aspect;
 
-import com.forgethson.springboot_test.Annotation.Record;
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
-import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
-
-import java.lang.reflect.Method;
 
 /**
  * 增强Record注解的切面类2（测试order顺序）
