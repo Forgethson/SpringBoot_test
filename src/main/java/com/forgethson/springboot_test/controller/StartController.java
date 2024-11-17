38e81dbddc414a5c324f77390007f20213192e8e
Forgethson-RF Center <649343453@qq.com>
2024-11-17 22:11:05 +0800

    add /body api

--- src/main/java/com/forgethson/springboot_test/controller/StartController.java
+++ src/main/java/com/forgethson/springboot_test/controller/StartController.java
@@ -3,9 +3,7 @@ package com.forgethson.springboot_test.controller;
 import com.forgethson.springboot_test.annotation.Record;
 import com.forgethson.springboot_test.entity.User;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -33,7 +31,7 @@ public class StartController {
     @GetMapping("/pojo")
     public User getUser() {
         log.info("请求：/start/pojo");
-        return new User(1L, "用户1", "123456");
+        return new User(1L, "用户1", "123456", true);
     }
 
     // 返回list（转为JSON）
@@ -41,8 +39,8 @@ public class StartController {
     public List<User> getUserList() {
         log.info("请求：/start/list");
         List<User> userList = new ArrayList<>();
-        User user1 = new User(1L, "测试用户1", "aaad21414");
-        User user2 = new User(2L, "测试用户2", "114132412");
+        User user1 = new User(1L, "测试用户1", "aaad21414", true);
+        User user2 = new User(2L, "测试用户2", "114132412", true);
         userList.add(user1);
         userList.add(user2);
         return userList;
@@ -53,12 +51,18 @@ public class StartController {
     public Map<String, Object> getMap() {
         log.info("请求：/start/map");
         Map<String, Object> map = new HashMap<>(3);
-        User user = new User(1L, "测试用户1", "123456");
+        User user = new User(1L, "测试用户1", "123456", true);
         map.put("作者信息", user);
         map.put("爱好", "sleep");
         map.put("ID", "Forgethson");
         map.put("github", "https://github.com/Forgethson/");
         return map;
+    }
+
+    @PostMapping("/body")
+    public User testBody(@RequestBody User user) {
+        log.info(user.toString());
+        return user;
     }
 
     // 测试打印log
